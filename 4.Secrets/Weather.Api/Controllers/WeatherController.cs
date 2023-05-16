42d5c9e98e4b9c7c5ddb61070d0faf5bd495d10a
Fauly Coelho <faulybsb@gmail.com>
2023-05-16 12:49:00 -0300

    fix

--- 4.Secrets/Weather.Api/Controllers/WeatherController.cs
+++ 4.Secrets/Weather.Api/Controllers/WeatherController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Weather.Api.Services;
 
 namespace Weather.Api.Controllers
 {
@@ -7,5 +8,23 @@ namespace Weather.Api.Controllers
     [ApiController]
     public class WeatherController : ControllerBase
     {
+        private readonly IWeatherService _weatherService;
+
+        public WeatherController(IWeatherService weatherService)
+        {
+            _weatherService = weatherService;
+        }
+
+        [HttpGet("weather/{city}")]
+        public async Task<IActionResult> GetWeatherForCity(string city)
+        {
+            var weather = await _weatherService.GetCurrentWeatherAsync(city);
+            if (weather is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(weather);
+        }
     }
 }
