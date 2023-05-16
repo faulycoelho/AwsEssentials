98523bc6f1bac79c66bf4037bb872288c6499ac0
Fauly Coelho <faulybsb@gmail.com>
2023-05-16 11:55:23 -0300

    add AwsSecrets

---
+++ 4.Secrets/Assistant/Program.cs
@@ -0,0 +1,11 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManagerClient = new AmazonSecretsManagerClient();
+var request = new GetSecretValueRequest
+{
+    SecretId = "myApyKey"
+};
+
+var response = secretsManagerClient.GetSecretValueAsync(request);
+Console.WriteLine(response.Result.SecretString);
---
+++ 4.Secrets/Weather.Api/Controllers/WeatherController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherController : ControllerBase
+    {
+    }
+}
